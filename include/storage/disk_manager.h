3584832a322531cb1bac734347cc8336087d2522
raihahahan <mraihandev@gmail.com>
2025-11-12 13:17:13 +0800

    create working implementation of diskmanager

---
+++ include/storage/disk_manager.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstdint>
+#include <fstream>
+#include <string>
+#include <vector> 
+using page_id_t = int32_t;;
+
+class DiskManager {
+public:
+    explicit DiskManager(const std::string &db_file);
+    DiskManager(const DiskManager& other) = delete;
+    DiskManager& operator=(const DiskManager& other) = delete;
+    DiskManager(DiskManager&& other) = delete;
+    DiskManager& operator=(DiskManager&& other) = delete;
+
+    ~DiskManager();
+
+    void ReadPage(page_id_t page_id, char* page_data);
+    void WritePage(page_id_t page_id, const char* page_data);
+    page_id_t AllocatePage();
+    void DeallocatePage(page_id_t page_id);
+
+    int GetNumPages() const;
+
+private:
+    mutable std::fstream db_io_;
+    std::vector<page_id_t> free_list;
+    page_id_t next_page_id_;
+};
