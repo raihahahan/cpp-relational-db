db8408ca27a5c6602d33ccc6b47c881e405e15bb
raihahahan <mraihandev@gmail.com>
2025-11-15 13:22:02 +0800

    update frame to use uint8_t dirty bit

--- include/storage/buffer_manager/frame.h
+++ include/storage/buffer_manager/frame.h
@@ -1,11 +1,12 @@
-#include "disk_manager/disk_manager.h"
+#pragma once
+#include "storage/disk_manager/disk_manager.h"
 #define INVALID_PAGE_ID -1
 
 namespace db::storage {
 struct Frame {
     page_id_t page_id = INVALID_PAGE_ID;
     int pin_count = 0;
-    int dirty = 0; // 0 = not dirty, 1 = dirty
+    uint8_t dirty = 0; // 0 = not dirty, 1 = dirty
     char* data; // memory region for page content
 };
 }
