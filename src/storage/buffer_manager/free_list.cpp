99f622099a055f4aff0a984cf881d819e7dff3a9
raihahahan <mraihandev@gmail.com>
2025-11-15 13:23:15 +0800

    create free list

--- src/storage/buffer_manager/free_list.cpp
+++ src/storage/buffer_manager/free_list.cpp
@@ -1,3 +1,29 @@
 #include "storage/buffer_manager/free_list.h"
 #include "storage/buffer_manager/frame.h"
+#include "config/config.h"
 
+namespace db::storage {
+FreeList::FreeList() = default;
+
+void FreeList::add(Frame* frame) {
+    list_.push_back(frame);
+};
+
+Frame* FreeList::get() {
+    if (empty()) {
+        return nullptr;
+    }
+
+    Frame* frame = list_.back();
+    list_.pop_back();
+    return frame;
+};
+
+size_t FreeList::size() const noexcept {
+    return list_.size();
+};
+
+bool FreeList::empty() const noexcept {
+    return list_.empty();
+};
+}
