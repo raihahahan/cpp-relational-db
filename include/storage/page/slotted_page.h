42190846c719214ab5c9c55f657be2f360b949b8
raihahahan <mraihandev@gmail.com>
2025-12-21 04:12:52 +0400

    update slotted page

---
+++ include/storage/page/slotted_page.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <cstdint>
+#include <optional>
+#include <span>
+
+namespace db::storage {
+using page_id_t = int32_t;
+
+struct Slot {
+    uint16_t offset;
+    uint16_t length;
+};
+
+struct PageHeader {
+    uint16_t num_slots;
+    uint16_t free_space_offset;
+};
+
+class SlottedPage {
+public:
+    // initialisers
+    // 1. existing page
+    static SlottedPage FromBuffer(char* page_data);
+    // 2. new page
+    static void Init(char* page_data);
+    
+    // modifiers
+    std::optional<uint16_t> Insert(const char* data, std::size_t len);
+    std::optional<std::span<const char>> getbsize(uint16_t slot_id);
+    void Update(uint16_t slot_id, const char* new_data, std::size_t len);
+    bool Remove(uint16_t slot_id);
+
+private:
+    explicit SlottedPage(char* page_data);
+    PageHeader* GetHeader();
+    Slot* GetSlot(uint16_t slot_id);
+    char* _data; // non-owning pointer of actual data
+    size_t FreeSpace() const; 
+};
+}
