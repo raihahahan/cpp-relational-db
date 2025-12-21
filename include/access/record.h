80cd2ee0f9de18cdbedb95401426413956fd6929
raihahahan <mraihandev@gmail.com>
2025-12-21 04:13:27 +0400

    add record.h

---
+++ include/access/record.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "storage/page/slotted_page.h"
+#include <vector>
+
+namespace db::access {
+using page_id_t = db::storage::page_id_t;
+struct RID {
+    page_id_t page_id;
+    uint16_t slot_id;
+};
+
+struct Record {
+    RID rid;
+    std::vector<char> data;
+};
+}
