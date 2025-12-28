a313b8dc7d078f9e370da01dc4bfcc7acbffb3b8
raihahahan <mraihandev@gmail.com>
2025-12-28 10:41:07 +0300

    update catalog

--- src/catalog/catalog.cpp
+++ src/catalog/catalog.cpp
@@ -1,4 +1,6 @@
 #include "catalog/catalog.h"
+#include "config/config.h"
+#include <assert.h>
 namespace db::catalog {
 Catalog::Catalog(db::storage::BufferManager* bm,
                 db::storage::DiskManager* dm) :
@@ -12,22 +14,8 @@ void Catalog::Init() {
     BootstrapCatalogs(); 
 }
 
-// db operations
-db_id_t Catalog::CreateDatabase(const std::string& db_name) { 
-    // insert row into db_databases 
-    db_id_t db_id = AllocateDatabaseId();
-    _databases.value().Insert(DatabaseInfo{db_id, db_name});
-    return db_id;
-}
-
-std::optional<DatabaseInfo> Catalog::GetDatabase(const std::string& db_name) const {
-    auto db_info = _databases.value().Lookup(db_name);
-    return db_info;
-}
-
 // table operations
 table_id_t Catalog::CreateTable(
-        db_id_t db_id,
         const std::string& table_name,
         const std::vector<ColumnInfo>& columns
 ) {
@@ -36,69 +24,112 @@ table_id_t Catalog::CreateTable(
     auto hf = HeapFile::Create(_bm, _dm, fid);
     _tables.value().Insert(TableInfo{
                                 table_id,
-                                db_id,
                                 table_name,
                                 fid,
                                 hf.GetPageId()
                             });
+    
+    for (const auto& col : columns) {
+        _attributes.value().Insert(col);
+    }
+    return table_id;
 }
 
-std::optional<TableInfo> Catalog::LookupTable(
-                        db_id_t db_id,
-                        const std::string& table_name
-) const {
-    return _tables.value().Lookup(db_id, table_name);
+std::optional<TableInfo> Catalog::LookupTable(const std::string& table_name) {
+    return _tables.value().Lookup(table_name);
 }
 
-std::vector<ColumnInfo> Catalog::GetTableColumns(table_id_t table_id) const {
+std::vector<ColumnInfo> Catalog::GetTableColumns(table_id_t table_id) {
     return _attributes.value().GetColumns(table_id);
 }
 
 // bootstrapping
 bool Catalog::IsInitialised() const {
-
+    if (_dm->GetNumPages() == 0) return false;
+    char buf[config::PAGE_SIZE];
+    _dm->ReadPage(config::ROOT_PAGE_ID, buf);
+    auto* hdr = reinterpret_cast<const storage::DBHeaderPage*>(buf);
+    return hdr->magic == config::DB_MAGIC;
 }
 
-void Catalog::LoadCatalogs(db_id_t db_id) {
-    auto databases_info = LookupTable(db_id, DB_DATABASES_TABLE);
-    auto tables_info = LookupTable(db_id, DB_TABLES_TABLE);
-    auto attributes_info = LookupTable(db_id, DB_ATTRIBUTES_TABLE);
-    auto types_info = LookupTable(db_id, DB_TYPES_TABLE);
+void Catalog::LoadCatalogs() {
+    auto tables_info = LookupTable(DB_TABLES_TABLE);
+    auto attributes_info = LookupTable(DB_ATTRIBUTES_TABLE);
+    auto types_info = LookupTable(DB_TYPES_TABLE);
 
-    _databases.emplace(DatabasesCatalog(&HeapFile::Open(
-        _bm, 
-        _dm, 
-        DB_DATABASES_FILE_ID,
-        databases_info.value_or(INVALID_PAGE_ID).first_page_id
-    )));
-    
-    _tables.emplace(TablesCatalog(&HeapFile::Open(
+    auto table_hf = HeapFile::Open(
         _bm, 
         _dm, 
         DB_TABLES_FILE_ID,
-        tables_info.value_or(INVALID_PAGE_ID).first_page_id
-    )));
-    
-    _attributes.emplace(AttributesCatalog(&HeapFile::Open(
+        config::DB_TABLES_ROOT_PAGE_ID
+    );
+
+    auto attr_hf = HeapFile::Open(
         _bm, 
         _dm, 
         DB_ATTRIBUTES_FILE_ID,
-        attributes_info.value_or(INVALID_PAGE_ID).first_page_id
-    )));
-    
-    _types.emplace(TypesCatalog(&HeapFile::Open(
+        config::DB_ATTRIBUTES_ROOT_PAGE_ID
+    );
+
+    auto types_hf = HeapFile::Open(
         _bm, 
         _dm, 
         DB_TYPES_FILE_ID,
-        types_info.value_or(INVALID_PAGE_ID).first_page_id
-    )));
+        config::DB_TYPES_ROOT_PAGE_ID
+    );
+
+    _tables.emplace(TablesCatalog(table_hf));
+    _attributes.emplace(AttributesCatalog(attr_hf));
+    _types.emplace(TypesCatalog(types_hf));
 }
 
 void Catalog::BootstrapCatalogs() {
-    _databases.emplace(DatabasesCatalog(&HeapFile::Create(_bm, _dm, DB_DATABASES_FILE_ID)));
-    _tables.emplace(TablesCatalog(&HeapFile::Create(_bm, _dm, DB_TABLES_FILE_ID)));
-    _attributes.emplace(AttributesCatalog(&HeapFile::Create(_bm, _dm, DB_ATTRIBUTES_FILE_ID)));
-    _types.emplace(TypesCatalog(&HeapFile::Create(_bm, _dm, DB_TYPES_FILE_ID)));
+    // page 0
+    page_id_t pid = _dm->AllocatePage();
+    assert(pid == config::ROOT_PAGE_ID);
+
+    storage::DBHeaderPage hdr{ .magic = config::DB_MAGIC };
+    _dm->WritePage(0, reinterpret_cast<const char*>(&hdr));
+
+    // db_tables
+    page_id_t p1 = _dm->AllocatePage();
+    assert(p1 == config::DB_TABLES_ROOT_PAGE_ID);
+    {
+        auto* frame1 = _bm->request(p1);
+        access::HeapFile::InitHeapPage(frame1->data);
+        _bm->unpin(frame1);
+        _bm->mark_dirty(frame1);
+    }
+
+    // db_attributes
+    page_id_t p2 = _dm->AllocatePage();
+    assert(p2 == config::DB_ATTRIBUTES_ROOT_PAGE_ID);
+    {
+        auto* frame2 = _bm->request(p2);
+        access::HeapFile::InitHeapPage(frame2->data);
+        _bm->unpin(frame2);
+        _bm->mark_dirty(frame2);
+    }
+
+    // db_types
+    page_id_t p3 = _dm->AllocatePage();
+    assert(p3 == config::DB_TYPES_ROOT_PAGE_ID);
+    {
+        auto* frame3 = _bm->request(p3);
+        access::HeapFile::InitHeapPage(frame3->data);
+        _bm->unpin(frame3);
+        _bm->mark_dirty(frame3);
+    }
+
+    _tables.emplace(TablesCatalog(
+        HeapFile::Open(_bm, _dm, DB_TABLES_FILE_ID, config::DB_TABLES_ROOT_PAGE_ID)
+    ));
+    _attributes.emplace(AttributesCatalog(
+        HeapFile::Open(_bm, _dm, DB_ATTRIBUTES_FILE_ID, config::DB_ATTRIBUTES_ROOT_PAGE_ID)
+    ));
+    _types.emplace(TypesCatalog(
+        HeapFile::Open(_bm, _dm, DB_TYPES_FILE_ID, config::DB_TYPES_ROOT_PAGE_ID)
+    ));
 
     InsertBuiltinTypes();
     InsertCatalogMetadata();
@@ -119,37 +150,22 @@ void Catalog::InsertBuiltinTypes() {
 }
 
 void Catalog::InsertCatalogMetadata() {
-    // db_databases
-    _tables.value().Insert(TableInfo{
-        .table_id = DB_DATABASES_TABLE_ID,
-        .db_id = DEFAULT_DB_ID,
-        .table_name = DB_DATABASES_TABLE,
-        .heap_file_id = DB_DATABASES_FILE_ID,
-        .first_page_id = _databases->GetFirstPage()
-    });
-
-    _attributes.value().Insert({DB_DATABASES_TABLE_ID, 1, "db_id", 1, 1});
-    _attributes.value().Insert({DB_DATABASES_TABLE_ID, 2, "db_name", 2, 2});
-
     // db_tables
     _tables.value().Insert(TableInfo{
         .table_id = DB_TABLES_TABLE_ID,
-        .db_id = DEFAULT_DB_ID,
         .table_name = DB_TABLES_TABLE,
         .heap_file_id = DB_TABLES_FILE_ID,
         .first_page_id = _tables->GetFirstPage()
     });
 
     _attributes.value().Insert({DB_TABLES_TABLE_ID, 1, "table_id", 1, 1});
-    _attributes.value().Insert({DB_TABLES_TABLE_ID, 2, "db_id", 1, 2});
-    _attributes.value().Insert({DB_TABLES_TABLE_ID, 3, "table_name", 2, 3});
-    _attributes.value().Insert({DB_TABLES_TABLE_ID, 4, "heap_file_id", 1, 4});
-    _attributes.value().Insert({DB_TABLES_TABLE_ID, 5, "first_page_id", 1, 5});
+    _attributes.value().Insert({DB_TABLES_TABLE_ID, 2, "table_name", 2, 2});
+    _attributes.value().Insert({DB_TABLES_TABLE_ID, 3, "heap_file_id", 1, 3});
+    _attributes.value().Insert({DB_TABLES_TABLE_ID, 4, "first_page_id", 1, 4});
 
     // db_attributes
     _tables.value().Insert(TableInfo{
         .table_id = DB_ATTRIBUTES_TABLE_ID,
-        .db_id = DEFAULT_DB_ID,
         .table_name = DB_ATTRIBUTES_TABLE,
         .heap_file_id = DB_ATTRIBUTES_FILE_ID,
         .first_page_id = _attributes->GetFirstPage()
@@ -164,7 +180,6 @@ void Catalog::InsertCatalogMetadata() {
     // db_types
     _tables.value().Insert(TableInfo{
         .table_id = DB_TYPES_TABLE_ID,
-        .db_id = DEFAULT_DB_ID,
         .table_name = DB_TYPES_TABLE,
         .heap_file_id = DB_TYPES_FILE_ID,
         .first_page_id = _types->GetFirstPage()
@@ -172,11 +187,5 @@ void Catalog::InsertCatalogMetadata() {
 
     _attributes.value().Insert({DB_TYPES_TABLE_ID, 1, "type_id", 1, 1});
     _attributes.value().Insert({DB_TYPES_TABLE_ID, 2, "size", 1, 2});
-
-    // insert default database
-    _databases.value().Insert(DatabaseInfo{
-        .db_id = DEFAULT_DB_ID,
-        .db_name = DEFAULT_DB_NAME
-    });
 }
 }
--- src/catalog/catalog_tables.cpp
+++ src/catalog/catalog_tables.cpp
@@ -1,24 +1,8 @@
 #include "catalog/catalog_tables.h"
 
 namespace db::catalog {
-std::optional<DatabaseInfo> DatabasesCatalog::Lookup(std::string_view db_name) const {
-    for (auto it = _hf->begin(); it != _hf->end(); ++it) {
-        auto rec = *it;
-        auto bytes = std::span<const uint8_t>{
-            reinterpret_cast<const uint8_t*>(rec.data),
-            rec.size
-        };
-        
-        auto db = codec::DatabaseInfoCodec::Decode(bytes);
-        if (db.db_name == db_name) {
-            return db;
-        }
-    }
-    return std::nullopt;
-}
-
-std::optional<TableInfo> TablesCatalog::Lookup(db_id_t db_id, std::string_view table_name) const {
-    for (auto it = _hf->begin(); it != _hf->end(); ++it) {
+std::optional<TableInfo> TablesCatalog::Lookup(std::string_view table_name) {
+    for (auto it = _hf.begin(); it != _hf.end(); ++it) {
         auto rec = *it;
         auto bytes = std::span<const uint8_t>{
             reinterpret_cast<const uint8_t*>(rec.data),
@@ -33,9 +17,9 @@ std::optional<TableInfo> TablesCatalog::Lookup(db_id_t db_id, std::string_view t
     return std::nullopt;
 }
 
-std::vector<ColumnInfo> AttributesCatalog::GetColumns(table_id_t table_id) const {
+std::vector<ColumnInfo> AttributesCatalog::GetColumns(table_id_t table_id) {
     std::vector<ColumnInfo> res;
-    for (auto it = _hf->begin(); it != _hf->end(); ++it) {
+    for (auto it = _hf.begin(); it != _hf.end(); ++it) {
         auto rec = *it;
         auto bytes = std::span<const uint8_t>{
             reinterpret_cast<const uint8_t*>(rec.data),
@@ -50,9 +34,9 @@ std::vector<ColumnInfo> AttributesCatalog::GetColumns(table_id_t table_id) const
     return res;
 }
 
-std::vector<TypeInfo> TypesCatalog::GetTypes() const {
+std::vector<TypeInfo> TypesCatalog::GetTypes() {
     std::vector<TypeInfo> res;
-    for (auto it = _hf->begin(); it != _hf->end(); ++it) {
+    for (auto it = _hf.begin(); it != _hf.end(); ++it) {
         auto rec = *it;
         auto bytes = std::span<const uint8_t>{
             reinterpret_cast<const uint8_t*>(rec.data),
