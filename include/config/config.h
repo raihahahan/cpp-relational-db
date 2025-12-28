94d276066273900f082b71c6e74cd77b47b16334
raihahahan <mraihandev@gmail.com>
2025-12-28 10:40:11 +0300

    add extra config

--- include/config/config.h
+++ include/config/config.h
@@ -4,5 +4,12 @@
 namespace db::config {
     inline constexpr size_t PAGE_SIZE = 8192; // 8kB
     inline constexpr size_t BUFFER_POOL_SIZE = 100;
+    inline constexpr std::string DATA_PATH = "data/";
+    inline uint32_t DB_MAGIC = 0xDBDBDBDB;
+    inline uint32_t ROOT_PAGE_ID = 0;
+    constexpr uint32_t DB_TABLES_ROOT_PAGE_ID = 1;
+    constexpr uint32_t DB_ATTRIBUTES_ROOT_PAGE_ID = 2;
+    constexpr uint32_t DB_TYPES_ROOT_PAGE_ID = 3;
 }
+
    
