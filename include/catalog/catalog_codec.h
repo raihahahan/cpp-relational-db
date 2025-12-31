2e851b4b3ed1b0727169057d1d8eb6301a0b0b2f
raihahahan <mraihandev@gmail.com>
2025-12-31 16:31:53 +0400

    update catalog concepts

--- include/catalog/catalog_codec.h
+++ include/catalog/catalog_codec.h
@@ -11,31 +11,41 @@ concept FixedWidthSerializable =
     std::is_trivially_copyable_v<T> &&
     std::is_standard_layout_v<T>;
 
+template <typename T>
+concept VariableWidthSerializable =
+    requires (T v) {
+        { v.data() } -> std::convertible_to<const char*>;
+        { v.size() } -> std::convertible_to<size_t>;
+    };
+
+
 template <FixedWidthSerializable T>
-inline void WriteInt(std::vector<uint8_t>& buf, T v) {
+inline void WriteData(std::vector<uint8_t>& buf, T v) {
     uint8_t bytes[sizeof(T)];
     std::memcpy(bytes, &v, sizeof(v));
     buf.insert(buf.end(), bytes, bytes + sizeof(v));
 };
 
-inline void WriteString(std::vector<uint8_t>& buf, std::string_view s) {
-    WriteInt(buf, static_cast<uint32_t>(s.size()));
+template <VariableWidthSerializable T>
+inline void WriteData(std::vector<uint8_t>& buf, T v) {
+    WriteData(buf, static_cast<uint32_t>(v.size()));
     buf.insert(buf.end(),
-                reinterpret_cast<const uint8_t*>(s.data()),
-                reinterpret_cast<const uint8_t*>(s.data() + s.size()));
+                reinterpret_cast<const uint8_t*>(v.data()),
+                reinterpret_cast<const uint8_t*>(v.data() + v.size()));
 };
 
 template <FixedWidthSerializable T>
-inline T ReadInt(std::span<const uint8_t> buf, size_t& off) {
+inline T ReadData(std::span<const uint8_t> buf, size_t& off) {
     T res;
     std::memcpy(&res, buf.data() + off, sizeof(res));
     off += sizeof(res);
     return res;
 };
 
-inline std::string ReadString(std::span<const uint8_t> buf, 
+template <VariableWidthSerializable T>
+inline T ReadData(std::span<const uint8_t> buf, 
                                 size_t& off) {
-    auto len = ReadInt<uint32_t>(buf, off);
+    auto len = ReadData<uint32_t>(buf, off);
     std::string s(reinterpret_cast<const char*>(buf.data() + off), len);
     off += len;
     return s;
--- src/catalog/catalog_codec.cpp
+++ src/catalog/catalog_codec.cpp
@@ -3,58 +3,58 @@
 namespace db::catalog::codec {
 std::vector<uint8_t> TableInfoCodec::Encode(const TableInfo& row) {
     std::vector<uint8_t> buf;
-    utilities::WriteInt(buf, row.first_page_id);
-    utilities::WriteInt(buf, row.heap_file_id);
-    utilities::WriteInt(buf, row.table_id);
-    utilities::WriteString(buf, row.table_name);
+    utilities::WriteData(buf, row.first_page_id);
+    utilities::WriteData(buf, row.heap_file_id);
+    utilities::WriteData(buf, row.table_id);
+    utilities::WriteData(buf, row.table_name);
     return buf;
 }
 
 TableInfo TableInfoCodec::Decode(std::span<const uint8_t> bytes) {
     TableInfo t;
     size_t off = 0;
-    t.first_page_id = utilities::ReadInt<page_id_t>(bytes, off);
-    t.heap_file_id = utilities::ReadInt<file_id_t>(bytes, off);
-    t.table_id = utilities::ReadInt<table_id_t>(bytes, off);
-    t.table_name = utilities::ReadString(bytes, off);
+    t.first_page_id = utilities::ReadData<page_id_t>(bytes, off);
+    t.heap_file_id = utilities::ReadData<file_id_t>(bytes, off);
+    t.table_id = utilities::ReadData<table_id_t>(bytes, off);
+    t.table_name = utilities::ReadData<std::string>(bytes, off);
     return t;
 }
 
 std::vector<uint8_t> ColumnInfoCodec::Encode(const ColumnInfo& row) {
     std::vector<uint8_t> buf;
-    utilities::WriteInt(buf, row.col_id);
-    utilities::WriteString(buf, row.col_name);
-    utilities::WriteInt(buf, row.ordinal_position);
-    utilities::WriteInt(buf, row.table_id);
-    utilities::WriteInt(buf, row.type_id);
+    utilities::WriteData(buf, row.col_id);
+    utilities::WriteData(buf, row.col_name);
+    utilities::WriteData(buf, row.ordinal_position);
+    utilities::WriteData(buf, row.table_id);
+    utilities::WriteData(buf, row.type_id);
     return buf;
 }
 
 ColumnInfo ColumnInfoCodec::Decode(std::span<const uint8_t> bytes) {
     ColumnInfo c;
     size_t off = 0;
-    c.col_id = utilities::ReadInt<col_id_t>(bytes, off);
-    c.col_name = utilities::ReadString(bytes, off);
-    c.ordinal_position = utilities::ReadInt<uint16_t>(bytes, off);
-    c.table_id = utilities::ReadInt<table_id_t>(bytes, off);
-    c.type_id = utilities::ReadInt<type_id_t>(bytes, off);
+    c.col_id = utilities::ReadData<col_id_t>(bytes, off);
+    c.col_name = utilities::ReadData<std::string>(bytes, off);
+    c.ordinal_position = utilities::ReadData<uint16_t>(bytes, off);
+    c.table_id = utilities::ReadData<table_id_t>(bytes, off);
+    c.type_id = utilities::ReadData<type_id_t>(bytes, off);
     return c;
 }
 
 std::vector<uint8_t> TypeInfoCodec::Encode(const TypeInfo& row) {
     std::vector<uint8_t> buf;
-    utilities::WriteInt(buf, row.type_id);
-    utilities::WriteInt(buf, row.size);
-    utilities::WriteString(buf, row.type_name);
+    utilities::WriteData(buf, row.type_id);
+    utilities::WriteData(buf, row.size);
+    utilities::WriteData(buf, row.type_name);
     return buf;
 }
 
 TypeInfo TypeInfoCodec::Decode(std::span<const uint8_t> bytes) {
     TypeInfo t;
     size_t off = 0;
-    t.type_id = utilities::ReadInt<type_id_t>(bytes, off);
-    t.size = utilities::ReadInt<uint16_t>(bytes, off);
-    t.type_name = utilities::ReadString(bytes, off);
+    t.type_id = utilities::ReadData<type_id_t>(bytes, off);
+    t.size = utilities::ReadData<uint16_t>(bytes, off);
+    t.type_name = utilities::ReadData<std::string>(bytes, off);
     return t;
 }
 }
