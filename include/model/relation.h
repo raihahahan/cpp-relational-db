d5909b8e6d3ff715f8d1ca2e07f1412e1720a1c4
raihahahan <mraihandev@gmail.com>
2026-01-04 13:29:14 +0800

    add decode to relation and schema, values to catalog codec

--- include/model/relation.h
+++ include/model/relation.h
@@ -4,16 +4,22 @@
 #include <span>
 #include "access/record.h"
 #include "access/heap/heap_file.h"
+#include "access/heap/heap_iterator.h"
+#include "common/tuple.h"
 
 using RID = db::access::RID;
+using Record = db::access::Record;
 using HeapFile = db::access::HeapFile;
+using HeapIterator = db::access::HeapIterator;
+using Tuple = db::common::Tuple;
 
 namespace db::model {
 class Relation {
 public:
     explicit Relation(HeapFile hf);
     std::optional<RID> InsertRaw(std::span<const uint8_t> bytes, size_t len);
-
+    HeapIterator Begin();
+    virtual Tuple Decode(const Record& rec) const  = 0;
 protected:
     HeapFile _hf;
 };
--- src/catalog/catalog_codec.cpp
+++ src/catalog/catalog_codec.cpp
@@ -1,5 +1,6 @@
 #include "catalog/catalog_codec.h"
 #include "util/data.h"
+#include "catalog/catalog_bootstrap.h"
 
 namespace db::catalog::codec {
 std::vector<uint8_t> TableInfoCodec::Encode(const TableInfo& row) {
@@ -21,9 +22,22 @@ TableInfo TableInfoCodec::Decode(std::span<const uint8_t> bytes) {
     return t;
 }
 
+std::vector<Value> TableInfoCodec::ToValues(const TableInfo& row) {
+    std::vector<Value> values;
+    values.push_back(row.first_page_id);
+    values.push_back(row.heap_file_id);
+    values.push_back(row.table_id);
+    values.push_back(row.table_name);
+    return values;
+}
+
+common::Schema TableInfoCodec::GetSchema() { 
+   return TABLES_CATALOG_SCHEMA;
+}
+
+
 std::vector<uint8_t> ColumnInfoCodec::Encode(const ColumnInfo& row) {
     std::vector<uint8_t> buf;
-    util::data::WriteData(buf, row.col_id);
     util::data::WriteData(buf, row.col_name);
     util::data::WriteData(buf, row.ordinal_position);
     util::data::WriteData(buf, row.table_id);
@@ -34,13 +48,26 @@ std::vector<uint8_t> ColumnInfoCodec::Encode(const ColumnInfo& row) {
 ColumnInfo ColumnInfoCodec::Decode(std::span<const uint8_t> bytes) {
     ColumnInfo c;
     size_t off = 0;
-    c.col_id = util::data::ReadData<col_id_t>(bytes, off);
     c.col_name = util::data::ReadData<std::string>(bytes, off);
     c.ordinal_position = util::data::ReadData<uint16_t>(bytes, off);
     c.table_id = util::data::ReadData<table_id_t>(bytes, off);
     c.type_id = util::data::ReadData<type_id_t>(bytes, off);
     return c;
 }
+
+std::vector<Value> ColumnInfoCodec::ToValues(const ColumnInfo& row) {
+    std::vector<Value> values;
+    values.push_back(row.col_name);
+    values.push_back(row.ordinal_position);
+    values.push_back(row.table_id);
+    values.push_back(row.type_id);
+    return values;
+}
+
+common::Schema ColumnInfoCodec::GetSchema() { 
+   return ATTR_CATALOG_SCHEMA;
+}
+
 
 std::vector<uint8_t> TypeInfoCodec::Encode(const TypeInfo& row) {
     std::vector<uint8_t> buf;
@@ -58,4 +85,17 @@ TypeInfo TypeInfoCodec::Decode(std::span<const uint8_t> bytes) {
     t.type_name = util::data::ReadData<std::string>(bytes, off);
     return t;
 }
+
+std::vector<Value> TypeInfoCodec::ToValues(const TypeInfo& row) {
+    std::vector<Value> values;
+    values.push_back(row.size);
+    values.push_back(row.type_id);
+    values.push_back(row.type_name);
+    return values;
+}
+
+common::Schema TypeInfoCodec::GetSchema() { 
+   return TYPES_CATALOG_SCHEMA;
+}
+
 }
