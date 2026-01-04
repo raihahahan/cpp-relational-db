1e233402b1bb77651210cee5a70fa7c80c05bef1
raihahahan <mraihandev@gmail.com>
2026-01-04 15:01:20 +0800

    update seq scan to take in reference

--- src/executor/operators/seq_scan_op.cpp
+++ src/executor/operators/seq_scan_op.cpp
@@ -1,16 +1,16 @@
 #include "executor/operators/seq_scan_op.h"
 
 namespace db::executor {
-SeqScanOp::SeqScanOp(std::shared_ptr<model::Relation> rel)
-        : _rel{std::move(rel)} {};
+SeqScanOp::SeqScanOp(model::Relation& rel)
+        : _rel{rel} {};
 
 void SeqScanOp::Open() {
-    _iter = _rel->Begin();
+    _iter = _rel.Begin();
 }
 
 std::optional<Tuple> SeqScanOp::Next() {
     if (!_iter.HasNext()) return std::nullopt;
-    return _rel->Decode(_iter.Next());
+    return _rel.Decode(_iter.Next());
 }
 
 void SeqScanOp::Close() {}
