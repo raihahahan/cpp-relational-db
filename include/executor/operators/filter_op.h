20fa206b4784583f50412beabf7075a787152d18
raihahahan <mraihandev@gmail.com>
2026-01-04 18:41:26 +0800

    make filterop predicate constructable on the fly

--- include/executor/operators/filter_op.h
+++ include/executor/operators/filter_op.h
@@ -10,6 +10,12 @@ public:
     explicit FilterOp(std::unique_ptr<Operator> child,
                         Predicate pred);
     
+    template <typename F>
+    requires std::invocable<F, const common::Tuple&>
+    FilterOp(std::unique_ptr<Operator> child, F&& f)
+        : _child{std::move(child)},
+          _pred{Predicate{std::forward<F>(f)}} {}
+    
     void Open() override;
     std::optional<Tuple> Next() override;
     void Close() override;
